005a91d46c64ac00645c70acf362570f888c3c8f
vilxd <dmytrochursin@gmail.com>
2024-12-19 18:06:38 +0100

    Update router.js

--- router.js
+++ router.js
@@ -9,17 +9,14 @@ const router = (fastify, options) => {
     fastify.post("/create-post", async (req, res) => {
         const connectToDBPosts = req.server.mongo.db.collection("posts");
         const { title, description, date_of_creating, nameurl, author } = req.body;
-        await connectToDBPosts.insertOne({ title:title, description: description, date_of_creating: date_of_creating, nameurl:nameurl, author: author, likes: 0});
-
-        const connectToDBAuthors = req.server.mongo.db.collection("authors");
         try{
-            const getCount = await connectToDBAuthors.find({name: author}).project({countposts: 1}).toArray();
-            await connectToDBAuthors.updateOne({name: author}, {$set: {countposts: getCount[0].countposts + 1}});
-            return res.status(200).send()
+            await connectToDBPosts.insertOne({ title:title, description: description, date_of_creating: date_of_creating, nameurl:nameurl, author: author});
+            return res.status(200).send();
         }
         catch{
-            return res.status(406).send()
+            return res.status(406).send();
         }
+        
     })
 
     fastify.get("/:nameurl", async (req, res) => {
